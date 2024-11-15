fcd87f944abd1f19ff2bf95fa3458ece1f7864c4
minhalee <minha1002@gmail.com>
2024-11-15 17:48:09 +0900

    feat: 프로젝트 영역 전체적으로 멘트 다듬기

--- payload/project.ts
+++ payload/project.ts
@@ -9,33 +9,43 @@ const project: IProject.Payload = {
       where: '지마켓 (Gmarket)',
       descriptions: [
         {
-          content: '계층 구조로 모든 Database Table을 설계 및 생성',
+          content: '계층 구조를 기반으로 모든 Database Table을 설계 및 생성',
         },
         {
-          content: 'Redis를 Cache Layer로 도입해서 캐싱 전략을 설계',
+          content: 'Redis를 Cache Layer로 도입하여 캐싱 전략을 설계',
           descriptions: [
-            { content: 'Look aside 패턴으로 읽기 전략 수립' },
-            { content: 'Job Scheduler Refactor and Optimization' },
+            {
+              content: 'Look-Aside 패턴을 적용하여 읽기 전략 수립 및 주기적인 Cache Warming 구현',
+            },
+            {
+              content:
+                '쓰기 전략은 Write-Back(좋아요 등)과 Write-Around(피드, 댓글 등)을 상황에 따라 혼합 사용',
+            },
           ],
         },
         {
-          content: 'Spring 멀티 모듈을 적용하여 Admin, Aggregator를 구성',
+          content: 'Spring 멀티 모듈 구조를 적용하여 Admin과 Aggregator를 구성',
+        },
+        {
+          content: 'JPA의 타입 안정성을 강화하기 위해 QueryDSL을 적용',
+          descriptions: [
+            {
+              content:
+                '서브쿼리를 FROM 절에 사용할 수 없는 제한을 해결하기 위해 `@SubSelect` 어노테이션으로 Entity 매핑',
+            },
+          ],
         },
         {
           content: '게시글 조회수 기능 개발',
           href: 'https://minggu92.tistory.com/131',
         },
         {
-          content: '실시간 인기 꿀템 추천 개발',
+          content: '실시간 인기 꿀템 추천 기능 개발',
           href: 'https://minggu92.tistory.com/137',
         },
         {
-          content: '50%의 구매전환율 달성',
+          content: '50%의 구매 전환율 달성',
           weight: 'MEDIUM',
-          // descriptions: [
-          //   { content: '30% improvement in system resource efficiency' },
-          //   { content: 'Job Scheduler Refactor and Optimization' },
-          // ],
         },
       ],
     },
@@ -46,35 +56,40 @@ const project: IProject.Payload = {
       descriptions: [
         {
           content:
-            '기존에 SP(Stored Procedure)를 호출하는 형태 → JPA를 적용하고 복잡한 쿼리는 queryDSL로 구현',
+            '기존 SP(Stored Procedure) 호출 방식을 JPA로 전환, 복잡한 쿼리는 QueryDSL로 구현',
         },
         {
-          content: 'Spring Multi Module 구성 및 CQRS 패턴을 도입하여 비즈니스 로직의 의존성을 분리',
+          content: 'Spring 멀티 모듈 구조와 CQRS 패턴을 도입하여 비즈니스 로직의 의존성을 분리',
         },
         {
-          content: '중복 발급 방지를 위해 Redis 를 활용한 분산락 적용',
-          href: 'https://minggu92.tistory.com/137',
+          content: 'gRPC를 적용하여 네트워크 비용 절감 및 API 스키마 사양 확립',
         },
         {
-          content: 'gRPC를 적용하여 네트워크 비용 절감, api schema spec 확립',
-        },
-        {
-          content: 'Armeria 프레임워크를 도입',
+          content: 'Armeria 프레임워크 도입',
           descriptions: [
-            { content: '신규 gRPC와 기존 Restful API 모두 사용 가능하도록 설정' },
-            { content: 'Spring framework와 연동하여 러닝커브 감소' },
+            { content: '신규 gRPC와 기존 RESTful API의 공존 가능하도록 설정' },
+            { content: 'Spring Framework와 연동하여 러닝 커브를 최소화' },
           ],
         },
-        { content: 'Redis 데이터를 protobuf로 저장', href: 'https://minggu92.tistory.com/130' },
         {
-          content: '트랜잭션에 의존하고 있어 중복 발급 발생',
+          content: 'Redis 데이터를 Protobuf 형식으로 저장',
+          href: 'https://minggu92.tistory.com/130',
+        },
+        {
+          content: '트랜잭션 의존으로 발생한 중복 발급 문제를 개선',
           weight: 'MEDIUM',
           descriptions: [
             {
               content:
-                "이벤트 응모 테이블이 워낙 방대해 별도 테이블 'created_at' 컬럼에 unique index 적용으로 해소",
+                "방대한 이벤트 응모 테이블의 중복 문제를 해결하기 위해 'created_at' 컬럼에 unique index 적용하여 해결하고 있었음.",
             },
-            { content: '동시성 발생을 DB 트랜잭션에 의존하는 것보다 Kafka 도입을 제안' },
+            {
+              content: '중복 발급 방지와 같은 동시성 처리를 위해 Redis 기반의 분산 락을 적용',
+              href: 'https://minggu92.tistory.com/137',
+            },
+            {
+              content: '시스템 안정성과 고가용성을 위해 Kafka 도입을 제안 및 전략 수립',
+            },
           ],
         },
       ],
@@ -85,24 +100,28 @@ const project: IProject.Payload = {
       endedAt: '2023-12',
       where: '지마켓 (Gmarket)',
       descriptions: [
-        { content: '빅스마일데이 중 Redis 서버에서 장애 발생' },
         {
-          content:
-            '수백개의 pod가 roll out 되면서 모든 pod에서 로컬 캐시 생성 위해 HGETALL 커맨드 실행되는 이슈',
+          content: '빅스마일데이 중 Redis 서버에서 장애 발생',
+          descriptions: [
+            {
+              content:
+                '수백 개의 pod가 Roll Out 되며 모든 pod에서 로컬 캐시 생성을 위해 HGETALL 명령어를 실행하는 문제가 발생',
+            },
+          ],
         },
         {
           content:
-            '서버가 순차적으로 roll out 될 수 있도록 DeploymentConfig의 Max Surge와 Max unavailable값 수정',
+            'DeploymentConfig의 Max Surge와 Max Unavailable 값을 조정하여 서버가 순차적으로 Roll Out 되도록 수정',
         },
         {
           content:
-            '서버 시작시 HGETALL 커맨드로 전체 호출하는 것이 아니라 요청이 발생 할 때 HGET 으로 필요한 key만 조회 ',
+            '서버 시작 시 HGETALL로 전체 호출하는 대신, 요청 시점에 HGET으로 필요한 key만 조회하도록 변경',
         },
         {
-          content: '조회한 결과에 대해 로컬 캐시를 생성하고 TTL 설정하여 Read Through 전략을 채택',
+          content: '조회 결과를 로컬 캐시로 저장하고 TTL을 설정하여 Read-Through 전략을 적용',
         },
         {
-          content: 'p95 latency 200ms → 20ms 개선',
+          content: 'p95 Latency를 200ms에서 20ms로 개선',
           weight: 'MEDIUM',
         },
       ],
@@ -113,45 +132,52 @@ const project: IProject.Payload = {
       endedAt: '2023-10',
       where: '지마켓 (Gmarket)',
       descriptions: [
-        { content: '기획전은 템플릿화 되어 담당자가 직접 상품 전시 페이지를 만드는 비즈니스.' },
+        {
+          content: '기획전은 템플릿화된 상품 전시 페이지를 담당자가 직접 제작하는 비즈니스',
+        },
         {
           content: '페인포인트 분석',
           weight: 'MEDIUM',
           descriptions: [
             {
-              content:
-                '기획전의 이력관리는 이미지 캡쳐를 통해 NAS에 저장하여 담당자가 눈으로 비교 → ',
-              descriptions: [
-                { content: '이전 데이터와의 diff를 비교해 보여줄 수 있도록 전체 데이터를 저장' },
-              ],
-            },
-            {
-              content: '매번 수정할 때마다 해당 기획전의 연관 모듈 테이블에 Write 작업이 발생 → ',
+              content: '기획전 이력 관리는 이미지 캡처 후 NAS에 저장해 담당자가 육안으로 비교',
               descriptions: [
                 {
-                  content: '에디터의 내용 전체를 한 곳에 저장하기 위해 json, BSON 등의 타입을 검토',
+                  content:
+                    '이전 데이터와의 차이를 비교할 수 있도록 전체 데이터를 저장하여 diff를 제공',
                 },
               ],
             },
             {
-              content: '신규 컬럼이 추가되면 DB 승인 과정이 필요해 애자일한 개발에 허들 →',
+              content: '기획전 수정 시 연관 모듈 테이블에 Write 작업이 발생',
               descriptions: [
-                { content: 'DB 작업에 드는 리소스를 줄이고 구조변경에 유연한 NoSQL 사용' },
+                {
+                  content: '에디터 내용을 한 곳에 저장하기 위해 JSON 및 BSON 타입 검토',
+                },
+              ],
+            },
+            {
+              content: '신규 컬럼 추가 시 DB 승인 과정으로 애자일 개발에 장애 발생',
+              descriptions: [
+                {
+                  content: 'DB 작업 리소스를 줄이고 유연한 구조 변경을 위해 NoSQL 도입 검토',
+                },
               ],
             },
           ],
         },
-        { content: '위 페인포인트를 해결하기 위해 MongoDB 적용' },
         {
-          content:
-            '모듈의 Format 변경이 발생할 수 있으므로 Version 필드를 컬렉션마다 설정해 하위 호환성 확보',
+          content: '위 페인포인트를 해결하기 위해 MongoDB 도입',
         },
         {
           content:
-            'NoSQL의 DB 정합성 확보를 위해 Format이라는 별도 컬렉션을 만들어 Json Schema로 검증',
+            '모듈 Format 변경 가능성을 대비해 Version 필드를 컬렉션별로 설정하여 하위 호환성 확보',
         },
         {
-          content: '웹, 모바일, 태블릿 등 다양한 Front에 랜딩하기 위해 GraphQL 적용',
+          content: 'NoSQL 정합성을 확보하기 위해 Json Schema로 검증하는 별도 Format 컬렉션 생성',
+        },
+        {
+          content: '웹, 모바일, 태블릿 등 다양한 프론트로 랜딩하기 위해 GraphQL 적용',
         },
       ],
     },
@@ -161,20 +187,22 @@ const project: IProject.Payload = {
       endedAt: '2022-12',
       where: '지마켓 (Gmarket)',
       descriptions: [
-        { content: '기획전 내에서 OCR 모듈을 연동 작업' },
         {
-          content: '유저 인터랙션을 통해 다양한 시나리오 상정',
+          content: '기획전에 OCR 모듈 연동 작업 수행',
+        },
+        {
+          content: '다양한 사용자 인터랙션을 바탕으로 다양한 시나리오를 설계 및 검증',
         },
         {
           content:
-            '클라이언트에서 동시에 요청을 보내면 비동기로 응답할 수 있도록 `Promise.allSettled()` 사용',
+            '클라이언트에서 동시 요청 시 비동기 응답을 처리하기 위해 `Promise.allSettled()` 사용',
         },
         {
           content: '지마켓 기획전 OCR 테스트 영상',
           href: 'https://www.youtube.com/watch?v=x6buh-JAY60',
         },
         {
-          content: '웹 접근성, 시각장애인 커뮤니티에 많이 소개됨.',
+          content: '웹 접근성 및 시각장애인 커뮤니티에 소개되어 큰 호응을 얻음',
         },
       ],
     },
@@ -184,47 +212,44 @@ const project: IProject.Payload = {
       endedAt: '2023-12',
       where: '지마켓 (Gmarket)',
       descriptions: [
-        { content: '하루 평균 250만회 이상의 요청 수행' },
-        { content: 'Android, iOS Native 코드 소스 분석' },
+        { content: '하루 평균 250만 회 이상의 요청 처리' },
+        { content: 'Android 및 iOS Native 코드 소스 분석' },
         { content: '딥링크를 생성할 수 있는 어드민 시스템 개발' },
         {
           content:
-            '웹 링크의 Referrer, Cookie 등의 메트릭을 수집해 from - To 모니터링 시스템을 Federation 패턴으로 구축',
+            '웹 링크의 Referrer 및 Cookie 등 메트릭을 수집하여 From-To 모니터링 시스템을 Federation 패턴으로 구축',
         },
-        { content: 'Android 앱 설치 후 동선이 유실 되는 케이스 해결' },
-        { content: 'iOS 카카오톡 인앱 브라우저내 딥링크 집행 실패 이슈 해결' },
+        { content: 'Android 앱 설치 후 동선 유실 문제 해결' },
+        { content: 'iOS 카카오톡 인앱 브라우저 내 딥링크 실패 이슈 해결' },
       ],
     },
     {
-      title: 'AI 기반 생활기록부 진단 서비스 "Vibe On" 개발',
+      title: "AI 기반 생활기록부 진단 서비스 'Vibe On' 개발",
       startedAt: '2021-03',
       endedAt: '2022-03',
       where: '아이엠비씨스템 (IMB System)',
       descriptions: [
-        { content: '웹 개발 PL을 맡아 모든 작업 일정 관리' },
+        { content: '웹 개발 PL을 맡아 작업 일정 관리' },
         { content: '전체 데이터베이스 설계' },
-        { content: 'OAuth2.0 기반의 sns 간편 회원가입, 로그인 기능 개발' },
-        { content: '리눅스 서버 구축, 방화벽 및 네트워크 설정' },
+        { content: 'OAuth2.0 기반 SNS 간편 회원가입 및 로그인 기능 개발' },
+        { content: '리눅스 서버 구축 및 방화벽, 네트워크 설정' },
         { content: 'WAS 설정(Wildfly)' },
         {
-          content: '이벤트 진행 중 WAS가 server down되는 현상 발생 →',
+          content: '이벤트 중 WAS 서버 다운 현상 발생 →',
           descriptions: [
             {
-              content:
-                'Application 의 Connectio pool은 이상없었으나 WAS의 DB Connection Pool이 낮게 설정되어 있어 WAS가 down되는 것을 확인',
+              content: 'WAS의 DB Connection Pool 설정이 낮아 서버 다운 원인 확인 및 수정',
             },
           ],
         },
         {
-          content: 'AI가 생기부를 분석한 텍스트 데이터를 DB로 Insert 안되는 상황 발생 →',
+          content: 'AI 생기부 분석 텍스트 데이터 DB Insert 문제 발생 →',
           descriptions: [
             {
-              content:
-                'PSQL로 접속해 확인하니 텍스트 데이터가 너무 길고 방대해 insert 하는 동안Exclusive Lock으로 인해 Writing 작업에 대기열이 쌓이고 결국 DeadLock 현상 발생',
+              content: '텍스트 데이터 크기 과다로 DeadLock 현상 발생 확인',
             },
             {
-              content:
-                ' 데이터를 api 호출 한번에 insert 하는 것이 아니라 타겟 테이블을 분할하고 Queue 테이블을 만들어 요청이 순차적으로 처리 될 수 있도록 작업',
+              content: 'API 호출 시 데이터를 분할하여 Queue 테이블을 통해 순차적 처리로 문제 해결',
             },
           ],
         },
@@ -236,19 +261,19 @@ const project: IProject.Payload = {
       endedAt: '2021-02',
       where: '아이엠비씨스템 (IMB System)',
       descriptions: [
-        { content: '미얀마 신한 MFI 지점에서 사용되는 태블릿 & 웹 프로그램 제작' },
+        { content: '미얀마 신한 MFI 지점에서 사용되는 태블릿 및 웹 프로그램 제작' },
         { content: 'Spring 기반 자체 솔루션으로 웹 개발' },
-        { content: '공통코드 관리' },
-        { content: '미얀마어로 포팅 후 출력되는 보고서 개발' },
-        { content: '리눅스 서버 구축, 방화벽 및 네트워크 설정' },
+        { content: '공통 코드 관리 기능 개발' },
+        { content: '미얀마어 포팅 및 출력 보고서 개발' },
+        { content: '리눅스 서버 구축 및 방화벽, 네트워크 설정' },
         {
           content: 'Android Native App 개발',
           descriptions: [
-            { content: '오프라인 상태에서도 저장할 수 있도록 내장 SQLite 사용' },
-            { content: '서명 할 수 있는 Sign Pad 기능 개발' },
-            { content: '사진 캡쳐 및 저장기능(Glide 라이브러리 활용)' },
-            { content: '웹 서버와 Sync 기능 (htttp 통신)' },
-            { content: 'Loading bar 표시를 위해 코틀린으로 migration 및 Coroutine 적용' },
+            { content: '오프라인 저장을 위한 SQLite 내장 사용' },
+            { content: 'Sign Pad 기능 개발로 전자 서명 가능' },
+            { content: '사진 캡처 및 저장 기능 구현 (Glide 라이브러리 활용)' },
+            { content: '웹 서버와 동기화를 위한 HTTP 통신 구현' },
+            { content: 'Loading bar 표시를 위해 Kotlin 및 Coroutine 적용' },
           ],
         },
       ],
@@ -259,11 +284,11 @@ const project: IProject.Payload = {
       endedAt: '2020-08',
       where: '아이엠비씨스템 (IMB System)',
       descriptions: [
-        { content: 'xFrame이라는 툴을 이용해 100개 이상의 화면 및 데이터 바인딩 작업' },
-        { content: '오래되고 복잡한 쿼리의 최적화 및 튜닝 작업 보조' },
-        { content: 'Oracle 및 Sybase DGB에 있는 레거시 데이터 흐름도 파악 및 분석' },
-        { content: '어떤 데이터들이 Mart 테이블에 들어가야 하는지 배움' },
-        { content: 'TerraStream 툴을 이용하여 Batch job 관리 작업을 보조' },
+        { content: 'xFrame 툴을 이용한 100개 이상의 화면 및 데이터 바인딩 작업' },
+        { content: '복잡한 쿼리 최적화 및 튜닝 작업 지원' },
+        { content: 'Oracle 및 Sybase DGB 레거시 데이터 흐름도 분석' },
+        { content: 'Mart 테이블에 적합한 데이터 선별 및 설계 학습' },
+        { content: 'TerraStream 툴을 활용한 Batch Job 관리 작업 지원' },
       ],
     },
   ],
